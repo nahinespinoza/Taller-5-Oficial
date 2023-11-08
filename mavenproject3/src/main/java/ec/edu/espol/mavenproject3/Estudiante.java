39d09ce8cce1d07e509bd3258c0845e49fa1889b
Alex-Benites <136673432+Alex-Benites@users.noreply.github.com>
2023-11-08 17:02:59 -0500

    Commit 3

---
+++ mavenproject3/src/main/java/ec/edu/espol/mavenproject3/Estudiante.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.mavenproject3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author marle
+ */
+public class Estudiante {
+    
+    private List<Curso> cursosInscrito = new ArrayList();
+    private List<Curso> cursosEnEsperaDeInscripcion = new ArrayList();
+
+    
+    public void solicitarInscripcion(Curso curso){
+        
+    }
+    
+    public void realizarActividadSumativa(Curso curso, ActividadSumativa actividadSumativa){
+        
+    }
+
+    public Estudiante() {
+    }
+    
+}
